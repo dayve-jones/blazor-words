b8a5504d7b2fbf01aad248cf5c682f0651be7aae
dayve-jones <92058566+dayve-jones@users.noreply.github.com>
2021-10-06 23:46:26 +0300

    Update WordsStorage.cs

--- WordsBlazor/Data/WordsStorage.cs
+++ WordsBlazor/Data/WordsStorage.cs
@@ -11,7 +11,7 @@ namespace WordsBlazor.Data
 
         public static void Init()
         {
-            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"singular");
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"words");
 
             Words = System.IO.File.ReadAllLines(path).Where( x=> x.Length >= 3  && x.Length <= 6).Distinct().ToList();
         }        
